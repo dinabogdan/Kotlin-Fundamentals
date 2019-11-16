c946f175b9b71c265fd1f0c699b98f74cfeb4a68
bogdan.dina <bogdan.dina@orange.com>
2019-11-16 18:25:10 +0200

    first commit

---
+++ src/ro/telacad/ktplayground/basics/HelloWorld.kt
@@ -0,0 +1,31 @@
+package ro.telacad.ktplayground.basics2
+
+// lowerCamelCase
+fun sum(a: Int, b: Int) = a + b
+
+fun sumAndPrint(a: Int, b: Int) {
+    println(a + b)
+}
+
+// UpperCase
+
+class Person {
+
+}
+
+class SpecialPerson {
+
+}
+
+
+fun main(args: Array<String>) {
+
+    // lowerCamelCase
+
+    val firstName = "John"
+
+    val lastName = "Doe"
+
+
+    println(sum(1, 2))
+}
