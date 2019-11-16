07de9839271322eb7c2c9e0553953019810cf206
bogdan.dina <bogdan.dina@orange.com>
2019-11-16 20:27:03 +0200

    some math

---
+++ src/ro/telacad/ktplayground/basics/math/SomeMath.kt
@@ -0,0 +1,29 @@
+package ro.telacad.ktplayground.basics.math
+
+fun main(args: Array<String>) {
+
+    val a = 10
+    val b = 20
+    val c = 30
+
+    println(a + b)
+
+    val sum = a + c
+
+    println(sum)
+
+    val diff = b - a
+
+    println(diff)
+
+    val multiply = a * b
+    println("Multiply : $multiply")
+
+    val div = b / a
+    println("Div $div")
+
+    println(a == b)
+
+    val d = a
+    println(a == d)
+}
