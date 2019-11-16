db73606731f94e711b015761d1ee5d0d33f5be4e
bogdan.dina <bogdan.dina@orange.com>
2019-11-16 18:54:12 +0200

    basic types

---
+++ src/ro/telacad/ktplayground/basics/types/BasicTypes.kt
@@ -0,0 +1,43 @@
+package ro.telacad.ktplayground.basics.types
+
+fun main(args: Array<String>) {
+
+    // Integers
+
+    // Byte 8 bit -128 127
+    val aByte: Byte = 1
+
+    // Short 16 bit -32768 32767
+    val aShort: Short = 32000
+
+    // Int 32 bit -2^32 (2^32)-1
+    val anInt: Int = 10
+
+    // Long 64 bit -2^64 (2^64)-1
+    val aLong: Long = 1000
+
+    // Decimals / Floating points numbers
+
+    // Float 32 bit
+
+    val aFloat: Float = 10F
+
+    // Double 64 bit
+
+    val aDouble: Double = 100.5
+
+    // Boolean true/false
+
+    val trueBoolean: Boolean = true
+    val falseBoolean = false
+
+    // Characters
+
+    // Char
+    val someChar = 'A'
+
+    // String
+    val someLongChar = "AB"
+
+
+}
