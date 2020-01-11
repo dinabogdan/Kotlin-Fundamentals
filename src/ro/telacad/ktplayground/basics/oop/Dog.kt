bcc2ee745e52bec3f23259591ef1c2e33655ad22
dinabogdan <bogdan.dina03@gmail.com>
2020-01-11 13:36:15 +0200

    accessing properties

--- src/ro/telacad/ktplayground/basics/oop/Dog.kt
+++ src/ro/telacad/ktplayground/basics/oop/Dog.kt
@@ -1,11 +1,11 @@
 package ro.telacad.ktplayground.basics.oop
 
 class Dog(
-    val name: String,
+    var name: String,
     val weight: Int,
     val breed: String
 ) {
-    
+
     fun bark() {
         if (weight < 20) {
             println("Small barking, something like yip!!")
@@ -19,5 +19,12 @@ fun main() {
     val dog = Dog(name = "Spark", weight = 19, breed = "Mixed")
 
     dog.bark()
+
+    println("The dog name is: ${dog.name}")
+
+    dog.name = "Tom"
+
+    println("The dog name is: ${dog.name}")
+
 
 }
