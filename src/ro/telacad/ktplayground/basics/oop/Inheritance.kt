aadb507f86dbaa2b3d39a0e41ed39eceaf127718
dinabogdan <bogdan.dina03@gmail.com>
2020-01-11 18:50:09 +0200

    add abstract classes

--- src/ro/telacad/ktplayground/basics/oop/Inheritance.kt
+++ src/ro/telacad/ktplayground/basics/oop/Inheritance.kt
@@ -4,22 +4,16 @@ package ro.telacad.ktplayground.basics.oop
 /**
  * Superclass
  */
-open class Animal {
+abstract class Animal() {
     open val name: String = ""
     open val food: String = ""
     open val habitat: String = ""
 
-    open fun makeNoise() {
-        println("The Animal is making noise!")
-    }
+    abstract fun makeNoise()
 
-    open fun eat() {
-        println("The Animal is eating!")
-    }
+    abstract fun eat()
 
-    open fun sleep() {
-        println("The animal is sleeping!")
-    }
+    abstract fun sleep()
 }
 
 /**
@@ -46,6 +40,47 @@ class Hippo : Animal() {
     }
 }
 
+abstract class WildCanine : Animal() {
+    override val food = "Meat"
+    override val habitat = "Forest"
+}
+
+class Fox : WildCanine() {
+    override val name = "Foxy"
+//    override val food = "Meat"
+//    override val habitat = "Forest"
+
+    override fun makeNoise() {
+        println("Fox $name is making noise")
+    }
+
+    override fun eat() {
+        println("Fox $name is eating $food")
+    }
+
+    override fun sleep() {
+        println("Fox $name is eating $food")
+    }
+}
+
+class Wolf : WildCanine() {
+    override val name = "Teddy"
+//    override val food = "Meat"
+//    override val habitat = "Forest"
+
+    override fun makeNoise() {
+        println("Wolf $name is making noise")
+    }
+
+    override fun eat() {
+        println("Wolf $name is eating $food")
+    }
+
+    override fun sleep() {
+        println("Wolf $name is eating $food")
+    }
+}
+
 class Veterinary {
     fun giveShot(animal: Animal) {
         println("The veterinary is giving a shot to ${animal.name}")
@@ -54,15 +89,18 @@ class Veterinary {
 }
 
 fun main() {
-    val animal = Animal()
-    animal.makeNoise()
-
     val hippo = Hippo()
-    val hippoAnimal: Animal = Hippo()
-    hippo.makeNoise()
-    hippoAnimal.makeNoise()
+    val wolf = Wolf()
+    val fox = Fox()
 
-    val animals: Array<Animal> = arrayOf(animal, hippo)
+    fox.eat()
+    wolf.eat()
+
+//    val hippoAnimal: Animal = Hippo()
+//    hippo.makeNoise()
+//    hippoAnimal.makeNoise()
+
+    val animals: Array<Animal> = arrayOf(wolf, fox, hippo)
     val veterinary = Veterinary()
     for (a in animals) {
         veterinary.giveShot(a)
