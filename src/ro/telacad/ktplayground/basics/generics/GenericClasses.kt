4b125522e7f81ffdd42a00bd2350e3dd373d9442
dinabogdan <bogdan.dina03@gmail.com>
2020-02-10 22:06:51 +0200

    add generic classes

---
+++ src/ro/telacad/ktplayground/basics/generics/GenericClasses.kt
@@ -0,0 +1,28 @@
+package ro.telacad.ktplayground.basics.generics
+
+data class Dog(
+    val id: Int,
+    val name: String,
+    val color: String
+)
+
+data class Cat(
+    val id: Int,
+    val name: String,
+    val color: String
+)
+
+class Cage<T>(
+    val animal: T,
+    val size: Double
+)
+
+fun main() {
+    val spark = Dog(1, "Spark", "Black")
+    val tom = Cat(1, "Tom", "Orange")
+    val sparkCage = Cage(spark, 1.0)
+    val tomCage = Cage(tom, 0.5)
+
+
+}
+
